306805751d530099d41ad8f7929bf06f9df2d759
Elias <elias.perez102@alu.ulpgc.es>
2021-03-14 22:14:31 +0000

    activity EditAccount

---
+++ app/src/main/java/es/ulpgc/da/fernando/foodieapp/EditAcoountActivity.java
@@ -0,0 +1,14 @@
+package es.ulpgc.da.fernando.foodieapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditAcoountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editAccount);
+    }
+}
