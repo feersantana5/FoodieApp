7167b32ed03eee30351dd9870df3b1b04f7d6e19
Elias <elias.perez102@alu.ulpgc.es>
2021-03-14 21:52:06 +0000

    activity editMenu

---
+++ app/src/main/java/es/ulpgc/da/fernando/foodieapp/EditMenuActivity.java
@@ -0,0 +1,14 @@
+package es.ulpgc.da.fernando.foodieapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editMenu);
+    }
+}
