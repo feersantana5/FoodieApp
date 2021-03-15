233b9a604d3e0ea0917efa1d3b95c17ab6806e87
Fernando <feersantana5@gmail.com>
2021-03-15 01:24:01 +0000

    añadido activuty

---
+++ app/src/main/java/es/ulpgc/da/fernando/foodieapp/RestaurantProfileActivity.java
@@ -0,0 +1,14 @@
+package es.ulpgc.da.fernando.foodieapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestaurantProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_profile);
+    }
+}
