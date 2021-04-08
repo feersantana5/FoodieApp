cefed2690954aded18093b9c434b9604bfacd171
Fernando <feersantana5@gmail.com>
2021-04-08 02:40:06 +0100

    intents

--- app/src/main/java/es/ulpgc/da/fernando/foodieapp/CreateMenuActivity.java
+++ app/src/main/java/es/ulpgc/da/fernando/foodieapp/CreateMenuActivity.java
@@ -1,14 +1,29 @@
 package es.ulpgc.da.fernando.foodieapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateMenuActivity extends AppCompatActivity {
 
+    Button buttonCreateMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createmenu);
+
+        buttonCreateMenu = findViewById(R.id.btnAddMenu);
+        buttonCreateMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
+
+
 }
--- app/src/main/java/es/ulpgc/da/fernando/foodieapp/EditAccountActivity.java
+++ app/src/main/java/es/ulpgc/da/fernando/foodieapp/EditAccountActivity.java
@@ -1,14 +1,29 @@
 package es.ulpgc.da.fernando.foodieapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EditAccountActivity extends AppCompatActivity {
 
+    Button buttonEditMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editaccount);
+
+        buttonEditMenu = findViewById(R.id.btnEditMenu);
+
+        buttonEditMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
+
 }
