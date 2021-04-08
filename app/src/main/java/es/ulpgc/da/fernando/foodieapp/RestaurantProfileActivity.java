2be008b2bbe462de0ff131346fba87584496b812
Elias <elias.perez102@alu.ulpgc.es>
2021-04-08 16:23:16 +0100

    Intent RestProfile bottom "mismenus"

--- app/src/main/java/es/ulpgc/da/fernando/foodieapp/RestaurantProfileActivity.java
+++ app/src/main/java/es/ulpgc/da/fernando/foodieapp/RestaurantProfileActivity.java
@@ -2,14 +2,34 @@ package es.ulpgc.da.fernando.foodieapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RestaurantProfileActivity extends AppCompatActivity {
 
+     Button myMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_profile);
+
+
+        myMenu = findViewById(R.id.mismenus);
+
+        myMenu.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                goToMyMenu();
+            }
+        });
+    }
+
+    private void goToMyMenu() {
+        Intent intent = new Intent(this, MyMenusActivity.class);
+        startActivity(intent);
     }
 
 }
