2c87af89848962be53b1f56a4f875d6c848f416c
Elias <elias.perez102@alu.ulpgc.es>
2021-04-09 21:24:13 +0100

    Formulario imagen en CreateMenu, arreglado fallo en los formularios

--- app/src/main/java/es/ulpgc/da/fernando/foodieapp/CreateMenuActivity.java
+++ app/src/main/java/es/ulpgc/da/fernando/foodieapp/CreateMenuActivity.java
@@ -1,19 +1,26 @@
 package es.ulpgc.da.fernando.foodieapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateMenuActivity extends AppCompatActivity {
 
-    Button buttonCreateMenu;
+    Button buttonCreateMenu,btnAddImag;
+    ImageView imagenNew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createmenu);
+
+        imagenNew = (ImageView) findViewById(R.id.imageView);
 
         buttonCreateMenu = findViewById(R.id.btnAddMenu);
         buttonCreateMenu.setOnClickListener(new View.OnClickListener() {
@@ -22,7 +29,30 @@ public class CreateMenuActivity extends AppCompatActivity {
                 finish();
             }
         });
+        btnAddImag= findViewById(R.id.btnAddImagen);
+        btnAddImag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cargarImagen();
+            }
+        });
 
+    }
+
+    private void cargarImagen() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("imagen/");
+        startActivityForResult(intent.createChooser(intent,"seleccione la Aplicacion"),10);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode ==RESULT_OK){
+            Uri path = data.getData();
+            imagenNew.setImageURI(path);
+        }
     }
 
 
